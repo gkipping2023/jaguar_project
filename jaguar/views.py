6a7f6145d250285ed680341da619cb6dcd7bb8bc
gkipping2023 <gkipping01@gmail.com>
2023-07-07 14:35:52 -0500

    first_commit

--- jaguar/views.py
+++ jaguar/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from store.models import Product
 
 # Create your views here.
 def home(request):
-    return render(request,'jaguar/home.html')
+    product = Product.objects.all()[0:6]
+    return render(request,'jaguar/home.html',{'product': product})
 
-def categories(request):
-    return render(request, 'jaguar/categories.html')
+def about (request):
+    return render(request, 'jaguar/about.html')
--- jaguar_project/settings.py
+++ jaguar_project/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'jaguar.apps.JaguarConfig',
+    'userprofile',
+    'store'
 ]
 
 MIDDLEWARE = [
